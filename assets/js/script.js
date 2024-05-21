aa3fe86e783826604de575a54716a26681252ee9
Danny <duongkhanhduy552@gmail.com>
2024-05-22 00:45:55 +0700

    update ccoding

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,76 @@
     // #page-top a khi được click sẽ cuộn về đầu trang
   });
 })(jQuery);
+
+
+class ImageResize {
+  constructor(config) {
+    const { width, height, element } = config;
+    this.imageW = width;
+    this.imageH = height;
+    this.imageMap = document.querySelector(element);
+    const mapId = this.imageMap.getAttribute("usemap");
+    const mapElem = `map[name="${mapId.substring(1, mapId.length)}"]`;
+    const area = document.querySelector(mapElem).children;
+    this.areaArray = Array.from(area);
+
+    window.addEventListener("load", this.reloadEvent);
+    window.addEventListener("resize", this.resizeEvent);
+    setTimeout(this.imgMap, 500);
+  }
+
+  getCoordinates = (elem) => {
+    let areaCords = elem.dataset.coords;
+
+    if (!areaCords) {
+      areaCords = elem.getAttribute("coords");
+
+      elem.dataset.coords = areaCords;
+    }
+
+    return areaCords;
+  };
+
+  imgMap = () => {
+    this.wPercent = this.imageMap.offsetWidth / 100;
+    this.hPercent = this.imageMap.offsetHeight / 100;
+
+    this.areaArray.forEach(this.areaLoop);
+  };
+
+  areaLoop = (area) => {
+    const coordinates = this.getCoordinates(area).split(",");
+    const coordsPercent = coordinates.map(this.mapCoords).join();
+    area.setAttribute("coords", coordsPercent);
+  };
+
+  mapCoords = (coordinate, index) => {
+    const parseCord = parseInt(coordinate, 10);
+
+    return index % 2 === 0
+      ? this.coordinatesMath(parseCord, this.imageW, this.wPercent)
+      : this.coordinatesMath(parseCord, this.imageH, this.hPercent);
+  };
+
+  coordinatesMath = (coordinates, imgVal, percentVal) =>
+    (coordinates / imgVal) * 100 * percentVal;
+
+  reloadEvent = () => {
+    this.imgMap();
+    // hoverBtn();
+  };
+
+  resizeEvent = () => {
+    this.imgMap();
+  };
+}
+
+// Khởi tạo ImageResize cho PC và mobile
+var eleImageMap = document.getElementById("jsImageMapPc");
+if (eleImageMap) {
+  const resizeImgPC = new ImageResize({
+    width: 1783,
+    height: 821,
+    element: "#jsImageMapPc",
+  });
+}
